6911293950e72da4e9e79bebef1ad0677060c717
kelly-luo <luo.kelly@hotmail.com>
2020-09-17 20:52:58 +1200

    Analysis for each categories TP and FP count

--- NER-Evaluation.py
+++ NER-Evaluation.py
@@ -8,6 +8,18 @@ from nltk.tree import Tree
 import nltk
 import os
 
+O_TP = 0
+OP_FP = 0
+OL_FP = 0
+
+P_TP = 0
+PO_FP = 0
+PL_FP = 0
+
+L_TP = 0
+LO_FP = 0
+LP_FP = 0
+
 TP = 0
 FN = 0
 FP = 0
@@ -46,17 +58,15 @@ def label_data(txt_writing):
         for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))): #for all the connected chunks
             if hasattr(chunk, 'label'):
                 if chunk.label() == 'GPE':
-                    # print(' '.join(c[0] for c in chunk) + " LOCATION")
-                    resultLabel = ' '.join(c[0] for c in chunk) , "LOCATION"
+                    resultLabel = ' '.join(c[0] for c in chunk), "LOCATION"
                     results_array.append(resultLabel)
                 else:
-                    # print(' '.join(c[0] for c in chunk) + chunk.label())
-                    resultLabel = ' '.join(c[0] for c in chunk) , chunk.label()
+                    resultLabel = ' '.join(c[0] for c in chunk), chunk.label()
                     results_array.append(resultLabel)
 
 
 def ner_evaluation_and_comparison():
-    global TP, FP, FN
+    global TP, FP, FN, O_FN, P_FN, L_FN
     datLineCount = 0
     for entry in results_array:
 
@@ -68,9 +78,11 @@ def ner_evaluation_and_comparison():
 
         if entry[0].lower() == dat_array[datLineCount][0].lower():  # Case 1: Text is the same
             if entry[1] == dat_array[datLineCount][1] or len(entry[1]) == len(dat_array[datLineCount][1]):  # Case 1.1: Labels are the same
+                correct_label_matrix_count(entry[1])
                 TP += 1
                 datLineCount += 1
             else:  # Case 1.1: Labels are different
+                wrong_label_matrix_count(((dat_array[datLineCount][1])[:1]).capitalize(), entry[1][:1])
                 FP += 1
                 datLineCount += 1
         elif entry[0].lower() in dat_array[datLineCount][0].lower() and len(entry[0]) < len(dat_array[datLineCount][0]):  # Case 1: Result text is missing a word
@@ -81,24 +93,57 @@ def ner_evaluation_and_comparison():
             datLineCount += 1
         else:
             for x in range(0,3):
-                if (datLineCount + x + 1) > len(dat_array): # stop the loop for checking other lines in dat file if there is no more lines
+                if (datLineCount + x + 1) > len(dat_array):  # stop the loop for checking other lines in dat file if there is no more lines
                     FP += 1
                     break
                 if entry[0].lower() == dat_array[datLineCount + x][0].lower():  # Case 1: Text is the same
                     if entry[1] == dat_array[datLineCount + x][1] or len(entry[1]) == len(dat_array[datLineCount + x][1]):  # Case 1.1: Labels are the same
+                        correct_label_matrix_count(entry[1])
                         TP += 1
                         datLineCount = datLineCount + x + 1
-                        if(x > 0): # if the results count is larger than the dat count that means it missed one
+                        if x > 0:  # if the results count is larger than the dat count that means it missed one
                             FN += x
                         break
                     else:  # Case 1.1: Labels are different
+                        wrong_label_matrix_count(((dat_array[datLineCount][1])[:1]).capitalize(), entry[1][:1])
                         FP += 1
                         datLineCount = datLineCount + x + 1
-                        if(x > 0): # if the results count is larger than the dat count that means it missed one
+                        if x > 0:  # if the results count is larger than the dat count that means it missed one
                             FN += x
                         break
                 if x == 4: # After searching next following 5 lines, cannot find right match
                     FP += 1
+
+
+def correct_label_matrix_count(results_label):
+    global O_TP, P_TP, L_TP
+
+    if results_label == 'ORGANIZATION':
+        O_TP += 1
+    elif results_label == 'PERSON':
+        P_TP += 1
+    elif results_label == 'LOCATION':
+        L_TP += 1
+
+
+def wrong_label_matrix_count(actual_label, pred_label):
+    global OP_FP, OL_FP, PO_FP, PL_FP, LO_FP, LP_FP
+
+    if actual_label == 'O':
+        if pred_label == 'P':
+            OP_FP += 1
+        elif pred_label == 'L' or pred_label == 'G':
+            OL_FP += 1
+    elif actual_label == 'P':
+        if pred_label == 'O':
+            PO_FP += 1
+        elif pred_label == 'L' or pred_label == 'G':
+            PL_FP += 1
+    elif actual_label == 'L' or actual_label == 'G':
+        if pred_label == 'O':
+            LO_FP += 1
+        elif pred_label == 'P':
+            LP_FP += 1
 
 
 def calculate_recall():
@@ -114,16 +159,27 @@ def calculate_Fvalue(precision, recall):
 
 
 def print_confusion_matrix():
-    print("------------- Confusion Matrix -------------")
-    confusionMatrix = array([' ', 'Actual Values'],
-                             [' ',str('Pos'), str('Neg')],
-                             [str('Pos'),'TP='+ str(TP), 'FP='+ str(FP)],
-                             [str('Pos'),'FN='+ str(FN), str('Pos')])
+    print("\r\n------------- Confusion Matrix -------------")
+    confusionMatrix = array([[str('   '), str('Pos'), str('Neg')],
+                             ['Pos','TP='+ str(TP), 'FP='+ str(FP)],
+                             ['Pos','FN='+ str(FN), str('Pos')]])
     print(confusionMatrix)
+
+    print("\r\n------------- Category Confusion Matrix -------------")
+    confusionMatrix = array([[str('   '), 'O', 'P', 'L'],
+                             ['O', str(O_TP), str(PO_FP) ,str(LO_FP)],
+                             ['P', str(OP_FP), str(P_TP) ,str(LP_FP)],
+                             ['L', str(OL_FP), str(PL_FP) ,str(L_TP)]])
+    print(confusionMatrix)
+
+    print("\r\nCategory Totals:")
+    print("ORGANISATION ------> TP:" + str(O_TP) + " ---- FP:" + (str(OP_FP + OL_FP)))
+    print("PERSON ------> TP:" + str(P_TP) + " ---- FP:" + (str(PO_FP + PL_FP)))
+    print("LOCATION ------> TP:" + str(L_TP) + " ---- FP:" + (str(LO_FP + LP_FP)))
 
 
 def print_FPR():
-    print("------------- FPR Calculations -------------")
+    print("\r\n------------- FPR Calculations -------------")
     precision = calculate_precision()
     print("Precision is: " + str(precision))
 
@@ -138,7 +194,7 @@ def read_dat_file(dat_writing):
 
         if line.strip() == '': # if the line is empty
             count += 1
-            print("SKIPPED LINE " + str(count))
+            print("---SKIPPED LINE " + str(count))
             continue
 
         l = line.split('(')
@@ -156,7 +212,8 @@ def read_dat_file(dat_writing):
 
         dat_array.append(l)
         count += 1
-        print("LINE " + str(count))
+        print("LINE " + str(count) + " EXTRACTED")
+
 
 # Set to your own dataset path with all the dat and txt files in the same folder
 datasetPath = 'C:/Users/LuoKe/OneDrive/Documents/EntireDataset/'
